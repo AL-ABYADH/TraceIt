41c730a56f56369ed4df6eebe3ce468af2e7aa78
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-05-25 00:03:44 +0300

    feat(all): initial setup

---
+++ packages/prettier-config/base.js
@@ -0,0 +1,12 @@
+/**
+ * A shared Prettier configuration for the repository.
+ *
+ * @type {import("prettier").Config}
+ * */
+export const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+};
