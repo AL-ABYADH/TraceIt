b47b4a509c5724b1e39f78f6b0a87714b9558776
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-05-31 00:14:59 +0300

    fix(server): add missing type definitions in project module

--- apps/server/src/features/project/controllers/project-collaboration/project-collaboration.controller.ts
+++ apps/server/src/features/project/controllers/project-collaboration/project-collaboration.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Delete, NotImplementedException, Param, Put } from "@nestjs/common";
 import { ProjectCollaborationService } from "../../services/project-collaboration/project-collaboration.service";
+import { ProjectCollaboration } from "../../entities/project-collaboration.entity";
 
 @Controller("project-collaborations")
 export class ProjectCollaborationController {
   constructor(private readonly projectCollaborationService: ProjectCollaborationService) {}
 
   @Put(":id")
-  async updateProjectCollaborationRoles() {
+  async updateProjectCollaborationRoles(): Promise<ProjectCollaboration> {
     throw new NotImplementedException();
   }
 
