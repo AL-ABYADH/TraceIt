59cf9ed7a93e7e72b4183341fcf5a7dd0080b8c7
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-05-30 23:50:18 +0300

    refactor(server): remove an unused dto in the user module

--- apps/server/src/features/user/dtos/create-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserInterface } from "../interfaces/create-user.interface";
-
-export class CreateUserDto implements CreateUserInterface {
-  username: string;
-  displayName: string;
-  email: string;
-  password: string;
-}
