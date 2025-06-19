af33d76e3ba1d0599611678657595a8b411349d4
GRZ07 <118290067+GRZ07@users.noreply.github.com>
2025-06-20 00:05:01 +0300

    feat(server): implement project permission repo functions

--- apps/server/src/features/project/repositories/project-permission/project-permission.repository.ts
+++ apps/server/src/features/project/repositories/project-permission/project-permission.repository.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotImplementedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { ProjectPermission } from "../../entities/project-permission.entity";
 import { CreateProjectPermissionInterface } from "../../interfaces/create-project-permission.interface";
 import { UpdateProjectPermissionInterface } from "../../interfaces/update-project-permission.interface";
 import { Neo4jService } from "src/core/neo4j/neo4j.service";
 import { ProjectPermissionModel } from "../../models/project-permission.model";
+import { plainToInstance } from "class-transformer";
 
 @Injectable()
 export class ProjectPermissionRepository {
@@ -14,25 +15,46 @@ export class ProjectPermissionRepository {
   }
 
   async create(projectPermission: CreateProjectPermissionInterface): Promise<ProjectPermission> {
-    throw new NotImplementedException();
+    const newProjectPermission = {
+      ...projectPermission,
+    };
+
+    const permission = await this.projectPermissionModel.createOne(newProjectPermission);
+    return plainToInstance(ProjectPermission, permission.getDataValues());
   }
 
   async getById(id: string): Promise<ProjectPermission> {
-    throw new NotImplementedException();
+    const projectPermission = await this.projectPermissionModel.findOne({ where: { id } });
+
+    return plainToInstance(ProjectPermission, projectPermission.getDataValues());
   }
 
   async getAll(): Promise<ProjectPermission[]> {
-    throw new NotImplementedException();
+    const projectPermissions = await this.projectPermissionModel.find({});
+    return projectPermissions.map((permission) =>
+      plainToInstance(ProjectPermission, permission.getDataValues()),
+    );
   }
 
   async update(
     id: string,
     projectPermission: UpdateProjectPermissionInterface,
   ): Promise<ProjectPermission> {
-    throw new NotImplementedException();
+    const [updatedProjectPermission] = await this.projectPermissionModel.update(
+      {
+        id,
+      },
+      projectPermission,
+    );
+
+    return plainToInstance(ProjectPermission, updatedProjectPermission.getDataValues());
   }
 
   async delete(id: string): Promise<boolean> {
-    throw new NotImplementedException();
+    const deleteResult = await this.projectPermissionModel.delete({
+      where: { id },
+    });
+
+    return deleteResult.count > 0;
   }
 }
