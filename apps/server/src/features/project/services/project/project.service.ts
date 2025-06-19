cb04484d08144c15e478106825410d91451efdd3
GRZ07 <118290067+GRZ07@users.noreply.github.com>
2025-06-20 00:06:18 +0300

    feat(server): implement project service functions

--- apps/server/src/features/project/services/project/project.service.ts
+++ apps/server/src/features/project/services/project/project.service.ts
@@ -1,37 +1,59 @@
-import { Injectable, NotImplementedException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { ProjectRepository } from "../../repositories/project/project.repository";
 import { Project } from "../../entities/project.entity";
 import { UpdateProjectInterface } from "../../interfaces/update-project.interface";
 import { CreateProjectParamsInterface } from "../../interfaces/create-project-params.interface";
+import { ProjectStatus } from "../../enums/project-status.enum";
+import { CreateProjectInterface } from "../../interfaces/create-project.interface";
 
 @Injectable()
 export class ProjectService {
   constructor(private readonly projectRepository: ProjectRepository) {}
 
   async create(project: CreateProjectParamsInterface): Promise<Project> {
-    throw new NotImplementedException();
-  }
-  async find(id: string): Promise<Project> {
-    throw new NotImplementedException();
+    const newProject: CreateProjectInterface = {
+      name: project.name,
+      description: project.description,
+      ownerId: project.userId,
+      createdAt: new Date(),
+    };
+
+    return this.projectRepository.create(newProject);
   }
 
-  async listUserProjects(userId: string): Promise<Project[]> {
-    throw new NotImplementedException();
+  async find(id: string): Promise<Project> {
+    const project = await this.projectRepository.getById(id);
+    if (!project) {
+      throw new NotFoundException(`Project with ID ${id} was not found`);
+    }
+    return project;
+  }
+
+  async listUserProjects(userId: string, status: ProjectStatus): Promise<Project[]> {
+    return this.projectRepository.getByOwnerOrCollaboration(userId, status);
   }
 
   async update(id: string, project: UpdateProjectInterface): Promise<Project> {
-    throw new NotImplementedException();
+    this.find(id);
+
+    return this.projectRepository.update(id, project);
   }
 
   async delete(id: string): Promise<boolean> {
-    throw new NotImplementedException();
+    this.find(id);
+
+    return this.projectRepository.delete(id);
   }
 
   async activate(id: string): Promise<boolean> {
-    throw new NotImplementedException();
+    this.find(id);
+
+    return this.projectRepository.setStatus(id, ProjectStatus.ACTIVE);
   }
 
   async archive(id: string): Promise<boolean> {
-    throw new NotImplementedException();
+    this.find(id);
+
+    return this.projectRepository.setStatus(id, ProjectStatus.ARCHIVED);
   }
 }
