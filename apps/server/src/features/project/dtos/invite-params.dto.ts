18b5db81e795b583308bf66b0298e06aee4538be
GRZ07 <118290067+GRZ07@users.noreply.github.com>
2025-07-23 12:44:52 +0300

    chore(server): remove unused dto and interface

--- apps/server/src/features/project/dtos/invite-params.dto.ts
+++
@@ -1,9 +0,0 @@
-import { InviteParamsInterface } from "../interfaces/invite-params.interface";
-
-export class InviteParamsDto implements InviteParamsInterface {
-  userId: string;
-  email: string | null;
-  username: string | null;
-  projectId: string;
-  projectRoleIds: string[];
-}
