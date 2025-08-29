8d5a8d6ee25613079d246b2c3f3e6f3f3ca8cffd
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-08-29 18:34:11 +0300

    chore(web): setup api url config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.API_BASE_URL + "/:path*",
+      },
+      {
+        source: "/socket.io/:path*",
+        destination: process.env.API_BASE_URL + "/socket.io/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
