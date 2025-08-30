6dbb2b11d91611bed7f988ef1f6a815bfbdffb81
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-08-31 00:17:36 +0300

    chore(web): setup tanstack query and redux toolkit with store and providers

---
+++ apps/web/src/providers/Providers.tsx
@@ -0,0 +1,16 @@
+// src/providers/Providers.tsx
+"use client";
+import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ReduxProvider>
+  );
+}
