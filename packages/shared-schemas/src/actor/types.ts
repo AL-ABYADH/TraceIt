c17cccded5d1c7bf9998783389ba5c691569553f
DrCinco730 <khaled775057605@gmail.com>
2025-09-08 05:36:02 +0300

    feat(packages): enhance actor schemas with type and subtype enums

--- packages/shared-schemas/src/actor/types.ts
+++ packages/shared-schemas/src/actor/types.ts
@@ -1,5 +1,12 @@
 import { z } from "../zod-openapi-init";
-import { addActorSchema, updateActorSchema } from "./schemas";
+import {
+  actorTypeSchema,
+  addActorSchema,
+  actorSubtypeActorSchema,
+  updateActorSchema,
+} from "./schemas";
 
 export type AddActorDto = z.infer<typeof addActorSchema>;
 export type UpdateActorDto = z.infer<typeof updateActorSchema>;
+export type SubTypeActorDto = z.infer<typeof actorSubtypeActorSchema>;
+export type ActorTypeDto = z.infer<typeof actorTypeSchema>;
