0672460af961962c19eb2f8c81336caf22d8979a
DrCinco730 <khaled775057605@gmail.com>
2025-09-08 06:21:32 +0300

    refactor(server): remove data mapping methods from actor repositories

--- apps/server/src/features/actor/repositories/interfaces/concrete-actor-repository.interface.ts
+++ apps/server/src/features/actor/repositories/interfaces/concrete-actor-repository.interface.ts
@@ -4,7 +4,7 @@ import { UpdateActorInterface } from "../../interfaces/update-actor.interface";
 
 export interface ConcreteActorRepositoryInterface<T extends Actor> {
   create(createActorInterface: CreateActorInterface): Promise<T>;
-  update(id: string, updateActorInterface: UpdateActorInterface): Promise<T>;
+  update(id: string, updateActorInterface: UpdateActorInterface): Promise<T[]>;
   delete(id: string): Promise<boolean>;
   getById(id: string): Promise<T | null>;
   getAll(): Promise<T[]>;
