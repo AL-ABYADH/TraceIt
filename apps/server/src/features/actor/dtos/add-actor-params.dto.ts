8385558a43fa1a1c8fc7b8dc305f32382c522efd
DrCinco730 <khaled775057605@gmail.com>
2025-09-09 20:15:28 +0300

    refactor(server): remove local actor DTOs in favor of shared schemas

--- apps/server/src/features/actor/dtos/add-actor-params.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ActorSubtype } from "../enums/actor-subtype.enum";
-import { AddActorParamsInterface } from "../interfaces/add-actor-params.interface";
-
-export class AddActorParamsDto implements AddActorParamsInterface {
-  name: string;
-  projectId: string;
-  subType: ActorSubtype;
-}
--- apps/server/src/features/actor/dtos/update-actor.dto.ts
+++
@@ -1,5 +0,0 @@
-import { UpdateActorInterface } from "../interfaces/update-actor.interface";
-
-export class UpdateActorDto implements UpdateActorInterface {
-  name: string;
-}
