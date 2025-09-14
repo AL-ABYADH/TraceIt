bb48086eca15db59fcd4fdbcf10724f51166a49e
Mareshen <mohammeduniverse9@gmail.com>
2025-09-14 12:07:41 +0300

    chore(server): remove tracked swagger subfolders, keep top-level files

--- apps/server/src/swagger/use-case/index.ts
+++
@@ -1,5 +0,0 @@
-// Module swagger index (auto-generated)
-import "./primary-use-case.openapi";
-import "./secondary-use-case.openapi";
-import "./use-case-diagram.openapi";
-import "./use-case.openapi";
