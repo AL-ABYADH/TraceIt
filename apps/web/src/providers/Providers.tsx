55226b4b087782e63582cd333581af67b6aa1a45
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-09-19 21:51:00 +0300

    chore(web): put query client in use state

--- apps/web/src/providers/Providers.tsx
+++ apps/web/src/providers/Providers.tsx
@@ -1,15 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "@/store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { wireTokenServiceToStore } from "@/modules/core/auth/store/auth-slice";
+import { defaultReactQueryOptions } from "@/query/query-options";
 
 const queryClient = new QueryClient();
 
 wireTokenServiceToStore(store);
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () => new QueryClient({ defaultOptions: defaultReactQueryOptions }),
+  );
+
   return (
     <ReduxProvider store={store}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
