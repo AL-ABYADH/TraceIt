2424614e1014a737f6eaf2842d355bfcb88bde2b
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-09-19 18:44:03 +0300

    fix(web): remove demo components & hooks that caused build to fail

--- apps/web/src/app/user-profile/page.tsx
+++ apps/web/src/app/user-profile/page.tsx
@@ -1,10 +1,7 @@
-import { UserProfile } from "@/modules/features/user/components/UserProfile";
-
 export default function UserProfilePage() {
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-4">User Profile Example</h1>
-      <UserProfile />
     </main>
   );
 }
