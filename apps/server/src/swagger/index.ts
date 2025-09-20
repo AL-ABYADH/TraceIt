9b726f144cf787ef46cdcf87ee84e5231589fecd
Mareshen <mohammeduniverse9@gmail.com>
2025-09-20 19:09:17 +0300

    chore(server): remove index.ts from version control

--- apps/server/src/swagger/index.ts
+++
@@ -1,9 +0,0 @@
-import "./auth/index";
-import "./user/index";
-import "./project/index";
-import "./actor/index";
-import "./use-case/index";
-import "./requirement/index";
-import { setupSwagger } from "./swagger-setup";
-
-export { setupSwagger };
