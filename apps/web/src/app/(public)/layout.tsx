9b1efb679899bd4437e17b3ca4a9e3ef741e0024
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-09-21 21:15:40 +0300

    feat(web): prevent showing login & signup pages if logged in

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -1,7 +1,13 @@
+import ClientAuthGate from "@/modules/core/auth/components/ClientAuthGate";
 import React from "react";
 
 export const metadata = { title: "Public" };
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div id="public-root" style={{ display: "none" }}>
+      <ClientAuthGate />
+      {children}
+    </div>
+  );
 }
--- apps/web/src/modules/core/auth/components/ClientAuthGate.tsx
+++ apps/web/src/modules/core/auth/components/ClientAuthGate.tsx
@@ -3,20 +3,29 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { RootState } from "@/store/store";
+import { usePathname } from "next/navigation";
 
 export default function ClientAuthGate() {
   const token = useSelector((s: RootState) => s.auth.token);
   const router = useRouter();
+  const pathname = usePathname() || "/";
 
   useEffect(() => {
-    const root = document.getElementById("private-root");
+    const privateRoot = document.getElementById("private-root");
+    const publicRoot = document.getElementById("public-root");
+
     if (!token) {
-      // if not authorized, redirect
+      // if not authorized, show auth layout, redirect to login, and return
+      if (publicRoot) publicRoot.style.display = "";
       router.replace("/login");
       return;
+    } else if (pathname === "/login" || pathname === "/signup") {
+      // if the path is login or signup, redirect to root
+      router.replace("/");
     }
-    // authorized -> reveal the content
-    if (root) root.style.display = "";
+
+    // if authorized -> reveal the content
+    if (privateRoot) privateRoot.style.display = "";
   }, [token, router]);
 
   return null;
