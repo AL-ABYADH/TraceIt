5fdd2380b44d7513915f5752f02fb99499192218
Mareshen <mohammeduniverse9@gmail.com>
2025-10-03 10:57:38 +0300

    feat(packages): add RequirementDto type with recursive nestedRequirements

--- packages/shared-schemas/src/requirement/types.ts
+++ packages/shared-schemas/src/requirement/types.ts
@@ -1,3 +1,5 @@
+import { actorSchema } from "../actor";
+import { useCaseDetailSchema } from "../use-case";
 import { z } from "../zod-openapi-init";
 import {
   childIdSchema,
@@ -17,29 +19,26 @@ import {
 
 export type UseCaseIdDto = z.infer<typeof useCaseIdSchema>;
 export type CreateRequirementDto = z.infer<typeof createRequirementSchema>;
-export type CreateRequirementExceptionDto = z.infer<
-  typeof createRequirementExceptionSchema
->;
+export type CreateRequirementExceptionDto = z.infer<typeof createRequirementExceptionSchema>;
 
 export type UpdateRequirementDto = z.infer<typeof updateRequirementSchema>;
 
-export type UpdateRequirementExceptionDto = z.infer<
-  typeof updateRequirementExceptionSchema
->;
+export type UpdateRequirementExceptionDto = z.infer<typeof updateRequirementExceptionSchema>;
 export type RequirementDetailDto = z.infer<typeof requirementDetailSchema>;
 export type RequirementListDto = z.infer<typeof requirementListSchema>;
-export type RequirementExceptionDetailDto = z.infer<
-  typeof requirementExceptionDetailSchema
->;
-export type RequirementExceptionListDto = z.infer<
-  typeof requirementExceptionListSchema
->;
+export type RequirementExceptionDetailDto = z.infer<typeof requirementExceptionDetailSchema>;
+export type RequirementExceptionListDto = z.infer<typeof requirementExceptionListSchema>;
 
 export type RequirementIdDto = z.infer<typeof requirementIdSchema>;
-export type RequirementExceptionIdDto = z.infer<
-  typeof requirementExceptionIdSchema
->;
+export type RequirementExceptionIdDto = z.infer<typeof requirementExceptionIdSchema>;
 
 export type ExceptionIdDto = z.infer<typeof exceptionIdSchema>;
 
 export type ChildIdDto = z.infer<typeof childIdSchema>;
+
+export type RequirementDto = z.infer<typeof requirementListSchema> & {
+  useCase: z.infer<typeof useCaseDetailSchema>;
+  actors?: z.infer<typeof actorSchema>[];
+  nestedRequirements?: RequirementDto[];
+  exceptions?: z.infer<typeof requirementExceptionListSchema>[];
+};
