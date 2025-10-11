abf2bf867237d035f84c6401a3979ee6fd06dcdb
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2025-10-11 17:20:16 +0300

    fix(web): remove a deleted import

--- apps/web/src/modules/core/auth/hooks/use-auth.ts
+++ apps/web/src/modules/core/auth/hooks/use-auth.ts
@@ -1,16 +1,14 @@
 "use client";
 import { RootState } from "@/store/store";
 import { useSelector, useDispatch } from "react-redux";
-import { logout as logoutAction, setUser } from "../store/auth-slice";
+import { logout as logoutAction } from "../store/auth-slice";
 
 export function useAuth() {
   const auth = useSelector((s: RootState) => s.auth);
   const dispatch = useDispatch();
   return {
-    user: auth.user,
     token: auth.token,
     isAuthenticated: !!auth.token,
-    setUser: (u: any) => dispatch(setUser(u)),
     logout: () => dispatch(logoutAction()),
   };
 }
